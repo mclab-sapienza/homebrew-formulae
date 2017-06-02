6c8e9b64ceb1f1c56af476f49ebdbc5b4cc85bb4
Stefano Sinisi <stefano.sinisi@gmail.com>
2017-06-02 12:36:24 +0200

    Fix use of spaces for indendation.

--- openmodelica.rb
+++ openmodelica.rb
@@ -50,22 +50,22 @@ class Openmodelica < Formula
 
   test do
     (testpath/"BouncingBall.mo").write <<-EOS.undent
-model BouncingBall "The 'classic' bouncing ball model"
-  type Height=Real(unit="m");
-  type Velocity=Real(unit="m/s");
-  parameter Real e=0.8 "Coefficient of restitution";
-  parameter Height h0=1.0 "Initial height";
-  Height h;
-  Velocity v;
-initial equation
-  h = h0;
-equation
-  v = der(h);
-  der(v) = -9.81;
-  when h<0 then
-    reinit(v, -e*pre(v));
-  end when;
-end BouncingBall;
+    model BouncingBall "The 'classic' bouncing ball model"
+      type Height=Real(unit="m");
+      type Velocity=Real(unit="m/s");
+      parameter Real e=0.8 "Coefficient of restitution";
+      parameter Height h0=1.0 "Initial height";
+      Height h;
+      Velocity v;
+    initial equation
+      h = h0;
+    equation
+      v = der(h);
+      der(v) = -9.81;
+      when h<0 then
+        reinit(v, -e*pre(v));
+      end when;
+    end BouncingBall;
     EOS
     system "#{bin}/omc", "-s", testpath/"BouncingBall.mo"
     system "make", "-f", "BouncingBall.makefile"
