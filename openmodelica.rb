f4ab419493d659ccc0b33db42af8ab9329a23912
Stefano Sinisi <stefano.sinisi@gmail.com>
2018-03-27 17:14:19 +0200

    Update to version 1.12.0

--- openmodelica.rb
+++ openmodelica.rb
@@ -1,40 +1,36 @@
 class Openmodelica < Formula
-  desc "Open-source Modelica-based modeling and simulation environment."
+  desc "Open-source Modelica-based modeling and simulation environment"
   homepage "https://openmodelica.org"
   url "https://github.com/OpenModelica/OpenModelica",
     :using => :git,
-    :tag => "v1.11.0",
-    :revision => "b44575c9591e5f7eac1928e135ad12ea995c3237"
-
-  bottle do
-    root_url "https://mclabservices.di.uniroma1.it/homebrew-science-bottles/"
-    sha256 "fe743e6893b436a218797c5d1daf9ab06eef8cb5104dbb475fcc355b737b0c00" => :sierra
-  end
+    :tag => "v1.12.0",
+    :revision => "2d85c3abb8486d728ffcbd3201095cf2552d8e47"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "cmake" => :build
-  depends_on "xz" => :build
   depends_on "gnu-sed" => :build
-  depends_on "qt" => "with-qtwebkit"
+  depends_on "libtool" => :build
+  depends_on "qt@5.5" => :build
+  depends_on "xz" => :build
   depends_on "gcc"
-  depends_on "lp_solve"
-  depends_on "readline"
-  depends_on "omniorb"
   depends_on "gettext"
   depends_on "hwloc"
-  depends_on "sundials" => :optional
+  depends_on "lp_solve"
+  depends_on "omniorb"
+  depends_on "open-mpi"
+  depends_on "readline"
   depends_on "boost" => :optional
+  depends_on "sundials" => :optional
 
   def install
     args = %W[--disable-debug
               --disable-dependency-tracking
               --disable-silent-rules
               --prefix=#{prefix}
-              CC=#{ENV.cc}
-              CXX=#{ENV.cxx}
-              --with-omniORB=#{Formula["omniorb"].opt_prefix}]
+              --with-omniORB=#{Formula["omniorb"].opt_prefix}
+              CXXFLAGS=-stdlib=libc++
+              LDFLAGS=-stdlib=libc++]
     args << "--with-cppruntime" if build.with? "boost"
     ohai "Checking out branch master for libraries submodule."
     cd("libraries") do
@@ -50,27 +46,26 @@ class Openmodelica < Formula
   end
 
   test do
-    (testpath/"BouncingBall.mo").write <<-EOS.undent
-    model BouncingBall "The 'classic' bouncing ball model"
-      type Height=Real(unit="m");
-      type Velocity=Real(unit="m/s");
-      parameter Real e=0.8 "Coefficient of restitution";
-      parameter Height h0=1.0 "Initial height";
-      Height h;
-      Velocity v;
-    initial equation
-      h = h0;
-    equation
-      v = der(h);
-      der(v) = -9.81;
-      when h<0 then
-        reinit(v, -e*pre(v));
-      end when;
-    end BouncingBall;
-    EOS
+    (testpath/"BouncingBall.mo").write <<-EOS
+model BouncingBall "The 'classic' bouncing ball model"
+  type Height=Real(unit="m");
+  type Velocity=Real(unit="m/s");
+  parameter Real e=0.8 "Coefficient of restitution";
+  parameter Height h0=1.0 "Initial height";
+  Height h;
+  Velocity v;
+initial equation
+  h = h0;
+equation
+  v = der(h);
+  der(v) = -9.81;
+  when h<0 then
+    reinit(v, -e*pre(v));
+  end when;
+end BouncingBall;
+EOS
     system "#{bin}/omc", "-s", testpath/"BouncingBall.mo"
     system "make", "-f", "BouncingBall.makefile"
-    assert File.exist?("BouncingBall")
     system "./BouncingBall"
   end
 end
